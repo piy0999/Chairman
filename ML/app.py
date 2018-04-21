3a9ae5125ff0940d6b3315930d07c7f9f0b89df8
Waqas Ali <waqas.abbasi@outlook.com>
2018-04-21 22:56:52 +0800

    spaces to tabs

--- ML/app.py
+++ ML/app.py
@@ -21,27 +21,27 @@ RGB_tuples = list(map(lambda x: tuple(255*np.array(colorsys.hsv_to_rgb(*x))), HS
 multiplier = 1
 
 def getFrame():
-    global frame
-    while True:
-        frame = video_capture.read()
+	global frame
+	while True:
+		frame = video_capture.read()
 
 def realtime():
-    while True:
-        for i in range(0, len(bounding_boxes)):
+	while True:
+		for i in range(0, len(bounding_boxes)):
 			b = bounding_boxes[i]
 			cv2.rectangle(frame, (b[0], b[1]), (b[2], b[3]), RGB_tuples[0], 6)
 			caption = "%s: %.1f%%"%(labels_to_names[0], scores[i]*100)
 			cv2.putText(frame, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 5)
 			cv2.putText(frame, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
-        cv2.imshow('frame',frame)
+		cv2.imshow('frame',frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            video_capture.release()
-            cv2.destroyAllWindows()
-            break
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			video_capture.release()
+			cv2.destroyAllWindows()
+			break
 def bound_boxes():
-     global bounding_boxes
+	 global bounding_boxes
 	 global scores
 	 while True:
 		_, img_encoded = cv2.imencode('.jpg', frame)
@@ -53,26 +53,26 @@ def bound_boxes():
 		scores = data['scores']
 
 if __name__ == "__main__":
-    global video_capture
-    global frame
-    global bounding_boxes
+	global video_capture
+	global frame
+	global bounding_boxes
 	global scores
-    bounding_boxes = []
-    control = 0
-    video_capture = cv2.VideoCapture('/Users/mengjiunchiou/keras-retinanet/videos/IMG_8766.MOV')
-    frame = video_capture.read()
+	bounding_boxes = []
+	control = 0
+	video_capture = cv2.VideoCapture('/Users/mengjiunchiou/keras-retinanet/videos/IMG_8766.MOV')
+	frame = video_capture.read()
 
-    gfthread = threading.Thread(target=getFrame, args='')
-    gfthread.daemon = True
-    gfthread.start()
+	gfthread = threading.Thread(target=getFrame, args='')
+	gfthread.daemon = True
+	gfthread.start()
 
-    rtthread = threading.Thread(target=realtime, args='')
-    rtthread.daemon = True
-    rtthread.start()
+	rtthread = threading.Thread(target=realtime, args='')
+	rtthread.daemon = True
+	rtthread.start()
 
-    fathread = threading.Thread(target=bound_boxes, args='')
-    fathread.daemon = True
-    fathread.start()
+	fathread = threading.Thread(target=bound_boxes, args='')
+	fathread.daemon = True
+	fathread.start()
 
 	while True: #keep main thread running while all three are non-daemon
-        pass
+		pass
